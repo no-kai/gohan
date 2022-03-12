db67199e77dd9d08da2a1aeb1891c484abaab1ac
no-kai <kaitaisuke@gmail.com>
2022-03-12 15:56:20 +0000

    [add] 画像投稿機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '89e05b1b2702285a2aee48f7f5f1217c84c6db643cbfa230741684175d79d8c6b1c244382f63fc15f363e51fee538f5695500c9b04a2d8af93a245e612e295e3'
